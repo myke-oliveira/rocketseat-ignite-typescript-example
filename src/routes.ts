a28957b4c27c9a2d95e2989452dd8eff6d179884
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2021-03-26 11:04:06 -0300

    🔨 REFACTOR: using data transfer object

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,11 @@ import { Request, Response } from "express";
 import CreateCourseService from "./CreateCourseService";
 
 export function createCourse(request: Request, response: Response): Response {
-  CreateCourseService.execute("NodeJs", 10, "Dani");
+  CreateCourseService.execute({
+    name: "NodeJs",
+    duration: 10,
+    educator: "Dani"
+  });
 
   return response.send();
 }
